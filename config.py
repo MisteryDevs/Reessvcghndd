c84dc343d132c869470c4815ba98b23b5f9f61a5
MisteryDevs <officialbotdev@gmail.com>
2025-09-24 08:07:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "8093150680"))
-SUPPORT_GRP = "BOTMINE_SUPPORT"
-UPDATE_CHNL = "BOTMINE_TECH"
-OWNER_USERNAME = "RADHE_XD"
+OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
+SUPPORT_GRP = "Ur_support07"
+UPDATE_CHNL = "Ur_Rishu_143"
+OWNER_USERNAME = "Vip_Robotz"
     
